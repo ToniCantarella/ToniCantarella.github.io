5c2e355ce001556f376228ca247951628a237f35
Toni Cantarella <toni.cantarella@outlook.com>
2025-03-02 18:52:12 +0200

    Basic react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,25 @@
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import './App.scss'
 
 function App() {
 
   return (
     <div id="app">
-      <div
-        id="navigation"
-      >
-        navigation
-      </div>
-      Application
+      <Router>
+        <div
+          id="navigation"
+        >
+          navigation
+          <Link to="/">app</Link>
+          <Link to="/test">test</Link>
+        </div>
+
+        <Routes>
+          <Route path='/' element={ <div>Application</div> }/>
+          <Route path='/test' element={ <div>test</div> }/>
+        </Routes>
+      </Router>
+      
     </div>
   )
 }
