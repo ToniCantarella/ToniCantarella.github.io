b301ba9557adf1860bd12c8219ec80e6634cbab7
Toni Cantarella <toni.cantarella@outlook.com>
2025-03-09 13:11:16 +0200

    Added framer motion to navigate with animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,55 @@
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link, useLocation } from "react-router-dom";
 import './App.scss'
 import { useState } from "react";
+import { AnimatePresence, motion } from "framer-motion";
+
+type PageProps = {
+  name: string
+}
+
+const Page = (props: PageProps) => {
+
+  return (
+    <motion.div
+      className="page"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0}}
+      transition={{ duration: .3 }}
+    >
+      {props.name}
+    </motion.div>
+  )
+}
+
+const AnimatedRoutes = () => {
+  const location = useLocation()
+
+  return (
+    <AnimatePresence mode="wait">
+      <Routes location={location} key={location.pathname}>
+        <Route
+          path='/'
+          element={
+            <Page
+              name={"Application"}
+            />
+          }
+        />
+        <Route
+          path='/test'
+          element={
+            <Page
+              name={"Test"}
+            />
+          }
+        />
+      </Routes>
+    </AnimatePresence>
+  )
+}
 
 function App() {
-
   const [darkTheme, setDarkTheme] = useState<Boolean>(true)
 
   return (
@@ -16,16 +62,14 @@ function App() {
           <Link to="/">app</Link>
           <Link to="/test">test</Link>
           <button onClick={() => setDarkTheme(!darkTheme)}>
-            theme switch 
+            theme switch
           </button>
+          {`${darkTheme}`}
         </div>
 
-        <Routes>
-          <Route path='/' element={ <div>Application</div> }/>
-          <Route path='/test' element={ <div>test</div> }/>
-        </Routes>
+        <AnimatedRoutes />
       </Router>
-      
+
     </div>
   )
 }
