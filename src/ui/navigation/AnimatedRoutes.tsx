3da7b09e418ba536269c38013c8a6fcad29cc6bd
Toni Cantarella <toni.cantarella@outlook.com>
2025-03-23 08:38:50 +0200

    Navigation animation no longer waits

--- src/ui/navigation/AnimatedRoutes.tsx
+++ src/ui/navigation/AnimatedRoutes.tsx
@@ -7,41 +7,43 @@ export const AnimatedRoutes = () => {
     const location = useLocation()
 
     return (
-        <AnimatePresence mode="wait">
-            <Routes location={location} key={location.pathname}>
-                <Route
-                    path={Pathnames.APP}
-                    element={
-                        <Page
-                            name={"Application"}
-                        />
-                    }
-                />
-                <Route
-                    path={Pathnames.TEST}
-                    element={
-                        <Page
-                            name={"Test"}
-                        />
-                    }
-                />
-                <Route
-                    path={Pathnames.TEST_SECOND}
-                    element={
-                        <Page
-                            name={"second test"}
-                        />
-                    }
-                />
-                <Route
-                    path={Pathnames.TEST_THIRD}
-                    element={
-                        <Page
-                            name={"third test"}
-                        />
-                    }
-                />
-            </Routes>
-        </AnimatePresence>
+        <div id="animated-routes">
+            <AnimatePresence>
+                <Routes location={location} key={location.pathname}>
+                    <Route
+                        path={Pathnames.APP}
+                        element={
+                            <Page
+                                name={"Application"}
+                            />
+                        }
+                    />
+                    <Route
+                        path={Pathnames.TEST}
+                        element={
+                            <Page
+                                name={"Test"}
+                            />
+                        }
+                    />
+                    <Route
+                        path={Pathnames.TEST_SECOND}
+                        element={
+                            <Page
+                                name={"second test"}
+                            />
+                        }
+                    />
+                    <Route
+                        path={Pathnames.TEST_THIRD}
+                        element={
+                            <Page
+                                name={"third test"}
+                            />
+                        }
+                    />
+                </Routes>
+            </AnimatePresence>
+        </div>
     )
 }
