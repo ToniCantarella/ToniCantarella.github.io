eb2c8521e07da5c0bd6c4deedf4cc7c758769a3d
Toni Cantarella <toni.cantarella@outlook.com>
2025-04-06 10:29:15 +0300

    Alignment and components for skills page

--- src/ui/Skills/Skills.tsx
+++ src/ui/Skills/Skills.tsx
@@ -1,13 +1,14 @@
 import { Page } from "../common/Page"
 import "./Skills.scss"
+import { Card } from "../common/Card"
 
 export const Skills = () => {
 
     return (
         <Page>
             <div id="skills">
-                <h1>Experience</h1>
                 <TimeLine />
+                <SkillList />
             </div>
         </Page>
     )
@@ -18,51 +19,69 @@ const TimeLine = () => {
     return (
         <div className="timeline">
 
-            <div className="timeperiod">
-                <div className="date">
-                    <h2>2022</h2>
-                </div>
-                <div className="foo">
-                    <div className="dot"></div>
-                    <div className="line"></div>
-                </div>
-                <div className="article">
-                    <h2>I learned a skill</h2>
-                    <h5>Where I learned it</h5>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam a vestibulum turpis, ac tristique massa. Curabitur et nisl quis lectus facilisis imperdiet ac et nibh. Proin porta blandit nunc id consectetur. In imperdiet mi sit amet venenatis ullamcorper. Donec mollis tortor id dolor blandit, ac scelerisque magna sodales. Vestibulum tincidunt finibus posuere. Aenean ut venenatis turpis. Vivamus metus sem, dignissim sed sagittis sit amet, lobortis vel sem. Phasellus id rhoncus massa, at euismod magna.</p>
-                </div>
-            </div>
+            <h1>Experience</h1>
 
-            <div className="timeperiod">
-                <div className="date">
-                    <h2>2023</h2>
-                </div>
-                <div className="foo">
-                    <div className="dot"></div>
-                    <div className="line"></div>
-                </div>
-                <div className="article">
-                    <h2>I learned a skill</h2>
-                    <h5>Where I learned it</h5>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam a vestibulum turpis, ac tristique massa. Curabitur et nisl quis lectus facilisis imperdiet ac et nibh. Proin porta blandit nunc id consectetur. In imperdiet mi sit amet venenatis ullamcorper. Donec mollis tortor id dolor blandit, ac scelerisque magna sodales. Vestibulum tincidunt finibus posuere. Aenean ut venenatis turpis. Vivamus metus sem, dignissim sed sagittis sit amet, lobortis vel sem. Phasellus id rhoncus massa, at euismod magna.</p>
-                </div>
-            </div>
+            <TimePeriod
+                year="2020"
+                title="Studies"
+                subtitle="Karelia"
+                paragraph="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vestibulum ex vitae mauris sodales, vitae rutrum lectus lobortis. Suspendisse egestas, justo quis faucibus faucibus, dolor metus semper purus, a convallis tellus elit in erat. Maecenas quis eleifend leo. Nulla ultrices in nisi eu varius. "
+            />
 
-            <div className="timeperiod">
-                <div className="date">
-                    <h2>2023</h2>
-                </div>
-                <div className="foo">
-                    <div className="dot"></div>
-                    <div className="line"></div>
-                </div>
-                <div className="article">
-                    <h2>I learned a skill</h2>
-                    <h5>Where I learned it</h5>
-                    <p>Lorem ipsum dolor sit amet, consectetSed quis imperdiet sem, convallis fringilla risus. Fusce laoreet eget sem vitae elementum. Integer ante eros, lobortis eu risus at, auctor porta eros. Fusce iaculis massa tellus. Pellentesque ut rutrum nunc, ac tincidunt augue. Integer commodo, magna nec finibus pretium, ipsum odio pellentesque eros, ut pretium sapien quam ut magna. Maecenas pharetra egestas lectus, eget pharetra dui mollis dignissim. Etiam velit purus, feugiat eget ullamcorper sit amet, finibus et ex. Proin scelerisque suscipit scelerisque. Nunc porta, lectus sit amet porttitor vehicula, purus nulla gravida lorem, sed aliquam leo justo dignissim magna. Aliquam justo sem, tempor elementum ligula vitae, tincidunt consectetur dolor. Nullam ex massa, sagittis ut eleifend in, molestie quis enim. Interdum et malesuada fames ac ante ipsum primis in faucibus. Sed facilisis, ligula sodales placerat consectetur, felis lectus facilisis dolor, in semper elit massa ut leo. Fusce eget vestibulum turpis. Integer imperdiet pulvinar est vitae efficitur.ur adipiscing elit. Aliquam a vestibulum turpis, ac tristique massa. Curabitur et nisl quis lectus facilisis imperdiet ac et nibh. Proin porta blandit nunc id consectetur. In imperdiet mi sit amet venenatis ullamcorper. Donec mollis tortor id dolor blandit, ac scelerisque magna sodales. Vestibulum tincidunt finibus posuere. Aenean ut venenatis turpis. Vivamus metus sem, dignissim sed sagittis sit amet, lobortis vel sem. Phasellus id rhoncus massa, at euismod magna.</p>
-                </div>
-            </div>
+            <TimePeriod
+                year="2022"
+                title="Internship"
+                subtitle="Sensire"
+                paragraph="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vestibulum ex vitae mauris sodales, vitae rutrum lectus lobortis. Suspendisse egestas, justo quis faucibus faucibus, dolor metus semper purus, a convallis tellus elit in erat. Maecenas quis eleifend leo. Nulla ultrices in nisi eu varius. Morbi ipsum ex, tristique vitae diam eu, varius iaculis nisi. Curabitur tincidunt mauris quis nisi faucibus pellentesque. Quisque finibus nisl et nunc lacinia iaculis."
+            />
 
+            <TimePeriod
+                year="2024"
+                title="hey"
+                subtitle="hello"
+                paragraph="Aliquam posuere ultricies tortor, in ullamcorper nibh porttitor eu. Phasellus rutrum nisi vel dui tempor sollicitudin. Ut augue ipsum, aliquet quis dignissim a, mattis vitae neque. Pellentesque semper fringilla accumsan. Mauris venenatis est quis elit porttitor elementum. Vestibulum faucibus, neque a varius ultrices, arcu felis tincidunt risus, eget maximus felis sem porta nisi. In posuere massa eget aliquam auctor. Aenean tristique luctus magna, a interdum justo consectetur ut. Aenean aliquet ex ut orci bibendum aliquam. Fusce tincidunt sed urna vel suscipit. Nunc accumsan orci non nunc sollicitudin eleifend. Suspendisse auctor ipsum a malesuada semper. Vestibulum sit amet risus est. Etiam nunc purus, consectetur a ornare nec, volutpat a mauris. Duis eget sodales elit. Duis et enim vel augue varius iaculis vel ut sem."
+            />
+
+        </div>
+    )
+}
+
+type TimePeriodProps = {
+    year: string,
+    title: string,
+    subtitle: string,
+    paragraph: string
+}
+
+const TimePeriod = (props: TimePeriodProps) => {
+    return (
+        <div className="timeperiod">
+            <div className="date">
+                <h2>{props.year}</h2>
+            </div>
+            <div className="foo">
+                <div className="dot"></div>
+                <div className="line"></div>
+            </div>
+            <div className="article">
+                <h2>{props.title}</h2>
+                <h5>{props.subtitle}</h5>
+                <p>{props.paragraph}</p>
+            </div>
+        </div>
+    )
+}
+
+const SkillList = () => {
+    return (
+        <div className="skill-list">
+            <h1>Skills</h1>
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
         </div>
     )
 }
