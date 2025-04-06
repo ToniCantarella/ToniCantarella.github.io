6135aa9d0c50d3460bdad2166f019c6a1d377543
Toni Cantarella <toni.cantarella@outlook.com>
2025-04-06 15:46:11 +0300

    Temp text for the timeline in skills page

--- src/ui/Skills/Skills.tsx
+++ src/ui/Skills/Skills.tsx
@@ -10,6 +10,7 @@ import ScssIcon from "../assets/scss.svg?react"
 import AndroidIcon from "../assets/android.svg?react"
 import KotlinIcon from "../assets/kotlin.svg?react"
 import GitIcon from "../assets/git.svg?react"
+import { useTranslation } from "react-i18next"
 
 
 export const Skills = () => {
@@ -25,33 +26,44 @@ export const Skills = () => {
 }
 
 const TimeLine = () => {
+    const { t } = useTranslation()
+
+    const currentDate = new Date()
+    const currentFormatted = `${currentDate.getMonth() + 1}/${currentDate.getFullYear()}`
 
     return (
         <div className="timeline">
-
-            <h1>Experience</h1>
-
+            <h1>{t("skills.experience")}</h1>
             <TimePeriod
-                year="2020"
-                title="Studies"
-                subtitle="Karelia"
-                paragraph="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vestibulum ex vitae mauris sodales, vitae rutrum lectus lobortis. Suspendisse egestas, justo quis faucibus faucibus, dolor metus semper purus, a convallis tellus elit in erat. Maecenas quis eleifend leo. Nulla ultrices in nisi eu varius. "
+                year="8/2020"
+                title={t("skills.studies")}
+                subtitle={t("skills.karelia-amk")}
+                paragraph={t("skills.studies-article")}
             />
-
             <TimePeriod
-                year="2022"
-                title="Internship"
-                subtitle="Sensire"
-                paragraph="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vestibulum ex vitae mauris sodales, vitae rutrum lectus lobortis. Suspendisse egestas, justo quis faucibus faucibus, dolor metus semper purus, a convallis tellus elit in erat. Maecenas quis eleifend leo. Nulla ultrices in nisi eu varius. Morbi ipsum ex, tristique vitae diam eu, varius iaculis nisi. Curabitur tincidunt mauris quis nisi faucibus pellentesque. Quisque finibus nisl et nunc lacinia iaculis."
+                year="5/2022"
+                title={t("skills.internship")}
+                subtitle={t("skills.sensire")}
+                paragraph={t("skills.internship-article")}
             />
-
             <TimePeriod
-                year="2024"
-                title="hey"
-                subtitle="hello"
-                paragraph="Aliquam posuere ultricies tortor, in ullamcorper nibh porttitor eu. Phasellus rutrum nisi vel dui tempor sollicitudin. Ut augue ipsum, aliquet quis dignissim a, mattis vitae neque. Pellentesque semper fringilla accumsan. Mauris venenatis est quis elit porttitor elementum. Vestibulum faucibus, neque a varius ultrices, arcu felis tincidunt risus, eget maximus felis sem porta nisi. In posuere massa eget aliquam auctor. Aenean tristique luctus magna, a interdum justo consectetur ut. Aenean aliquet ex ut orci bibendum aliquam. Fusce tincidunt sed urna vel suscipit. Nunc accumsan orci non nunc sollicitudin eleifend. Suspendisse auctor ipsum a malesuada semper. Vestibulum sit amet risus est. Etiam nunc purus, consectetur a ornare nec, volutpat a mauris. Duis eget sodales elit. Duis et enim vel augue varius iaculis vel ut sem."
+                year="12/2022"
+                title={t("skills.employed")}
+                subtitle={t("skills.sensire")}
+                paragraph={t("skills.employed-article")}
             />
-
+            <TimePeriod
+                year="6/2024"
+                title={t("skills.graduation")}
+                subtitle={t("skills.karelia-amk")}
+                paragraph={t("skills.graduation-article")}
+            />
+            <TimePeriod
+                year={currentFormatted}
+                title={t("skills.future")}
+                subtitle={t("skills.your-company")}
+                paragraph={t("skills.future-article")}
+            />
         </div>
     )
 }
@@ -85,12 +97,14 @@ const TimePeriod = (props: TimePeriodProps) => {
 }
 
 const SkillList = () => {
+    const { t } = useTranslation()
+
     return (
         <div className="skill-list">
-            <h1>Skills</h1>
+            <h1>{t("navigation.skills")}</h1>
             <SkillCard
                 label="React"
-                icon={<ReactIcon id="react"/>}
+                icon={<ReactIcon id="react" />}
             />
             <SkillCard
                 label="JS & TS"
