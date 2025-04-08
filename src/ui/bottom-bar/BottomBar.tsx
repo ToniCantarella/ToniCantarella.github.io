3c3295a649cb66debba824e57ac0515a94593b6f
Toni Cantarella <toni.cantarella@outlook.com>
2025-04-08 19:33:09 +0300

    Initial version of the bottom bar settings drawer with elements corresponding to topbar controls

--- src/ui/bottom-bar/BottomBar.tsx
+++ src/ui/bottom-bar/BottomBar.tsx
@@ -2,15 +2,22 @@ import "./BottomBar.scss"
 import { NavigationContext, Pages } from "../navigation/NavigationBar"
 import SettingsIcon from "../assets/settings.svg?react"
 import { useTranslation } from "react-i18next"
-import { useContext } from "react"
+import { useContext, useState } from "react"
 import { Link } from "react-router-dom"
+import { div } from "framer-motion/client"
+import { LanguagePicker } from "../language-picker/LanguagePicker"
+import { ThemePicker } from "../theme-picker/ThemePicker"
 
 export const BottomBar = () => {
+    const [settingsOpen, setSettingsOpen] = useState<boolean>(false)
     const navContext = useContext(NavigationContext)
     const { t } = useTranslation()
 
     return (
         <nav id="bottom-bar">
+            <SettingsDrawer
+                open={settingsOpen}
+            />
             {Pages.map(page =>
                 <Link
                     className={`bottom-bar-item ${navContext.currentPage === page.pathname ? "selected" : ""}`}
@@ -25,12 +32,31 @@ export const BottomBar = () => {
 
                 </Link>
             )}
-            <button className="bottom-bar-item settings">
+            <button
+                className="bottom-bar-item settings"
+                onClick={() => setSettingsOpen(!settingsOpen)}
+                onBlur={() => setSettingsOpen(false)}
+            >
                 <SettingsIcon />
                 <span>
                     {t("settings")}
                 </span>
             </button>
         </nav>
+    )
+}
+
+const SettingsDrawer = (props: { open: boolean }) => {
+
+    return (
+        <div
+            id="settings-drawer"
+            className={`${props.open ? "open" : ""}`}
+        >
+            <div id="content">
+                <ThemePicker />
+                <LanguagePicker />
+            </div>
+        </div>
     )
 }
