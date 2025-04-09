fb98ec2416b6ea4de446dbc2de1bce5ac8031fba
Toni Cantarella <toni.cantarella@outlook.com>
2025-04-09 19:00:43 +0300

    Examples card

--- src/ui/Examples/Examples.tsx
+++ src/ui/Examples/Examples.tsx
@@ -1,12 +1,50 @@
+import { useTranslation } from "react-i18next"
 import { Page } from "../common/Page"
+import "./Examples.scss"
+import { Card } from "../common/Card"
 
 export const Examples = () => {
+    const { t } = useTranslation()
 
     return (
         <Page>
-            <div>
-                exkmpls
+            <div id="examples">
+                <h1>{t("navigation.examples")}</h1>
+                <div className="content">
+                    <ExampleCard
+                        image={""}
+                        onClick={() => console.log("clicked")}
+                    />
+                </div>
             </div>
         </Page>
+    )
+}
+
+type ExampleCardProps = {
+    image: string,
+    onClick: () => void
+}
+
+const ExampleCard = (props: ExampleCardProps) => {
+
+    return (
+        <Card >
+            <>
+                <div
+                    className="image"
+                    style={{
+                        backgroundImage: `url(${props.image})`
+                    }}
+                />
+
+                <div
+                    className="card-content"
+                    onClick={props.onClick}
+                >
+                    <span>UI Elements</span>
+                </div>
+            </>
+        </Card>
     )
 }
