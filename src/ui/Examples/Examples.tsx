1cce321dd92af17b78ea3c9ef2bf038d9f04a465
Toni Cantarella <toni.cantarella@outlook.com>
2025-04-10 19:15:49 +0300

    Sections for examples page

--- src/ui/Examples/Examples.tsx
+++ src/ui/Examples/Examples.tsx
@@ -2,7 +2,7 @@ import { useTranslation } from "react-i18next"
 import { Page } from "../common/Page"
 import "./Examples.scss"
 import { Card } from "../common/Card"
-import { useState } from "react"
+import React, { useState } from "react"
 import { Dialog } from "../common/Dialog"
 
 export const Examples = () => {
@@ -15,29 +15,68 @@ export const Examples = () => {
                 <>
                     <div id="examples">
                         <h1>{t("navigation.examples")}</h1>
-                        <div className="content">
-                            <ExampleCard
-                                label={t("examples.ui-elements")}
-                                image={""}
-                                onClick={() => setDialogOpen(true)}
-                            />
+                        <div className="examples-content">
+
+                            <ExampleSection
+                                title={t("examples.ui-elements")}
+                            >
+                                <ExampleCard
+                                    label={t("examples.loading")}
+                                    image={""}
+                                    onClick={() => setDialogOpen(true)}
+                                />
+                                <ExampleCard
+                                    label={t("examples.loading")}
+                                    image={""}
+                                    onClick={() => setDialogOpen(true)}
+                                />
+                                <ExampleCard
+                                    label={t("examples.loading")}
+                                    image={""}
+                                    onClick={() => setDialogOpen(true)}
+                                />
+                            </ExampleSection>
+
+                            <ExampleSection
+                                title={t("examples.games")}
+                            >
+                                <ExampleCard
+                                    label={t("examples.loading")}
+                                    image={""}
+                                    onClick={() => setDialogOpen(true)}
+                                />
+                            </ExampleSection>
                         </div>
                     </div>
                     {dialogOpen &&
                         <Dialog
-                            title={t("examples.ui-elements")}
+                            title={t("examples.loading")}
                             onClose={() => setDialogOpen(false)}
                         >
                             <div>
-                                {t("examples.ui-elements")}
+                                {t("examples.loading")}
                             </div>
                         </Dialog>
                     }
                 </>
             </Page>
-
-
         </>
+    )
+}
+
+type ExampleSectionProps = {
+    title: string,
+    children: React.ReactNode
+}
+
+const ExampleSection = (props: ExampleSectionProps) => {
+    return (
+        <div className="examples-section">
+            <h2>{props.title}</h2>
+            <div className="examples-grid">
+                {props.children}
+            </div>
+        </div>
     )
 }
 
