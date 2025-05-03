c6513b0fd9d416cd64afde737de7ae9efbc238fc
Toni Cantarella <toni.cantarella@outlook.com>
2025-05-03 10:11:40 +0300

    Intro font size is unset on mobile

--- src/ui/navigation/bottom-bar/BottomBar.tsx
+++ src/ui/navigation/bottom-bar/BottomBar.tsx
@@ -5,6 +5,7 @@ import { useTranslation } from "react-i18next"
 import { useState } from "react"
 import { LanguagePicker } from "../../language-picker/LanguagePicker"
 import { ThemePicker } from "../../theme/theme-picker/ThemePicker"
+import { HomeButton } from "../home-button/HomeButton"
 
 export const BottomBar = () => {
     const [settingsOpen, setSettingsOpen] = useState<boolean>(false)
@@ -40,6 +41,7 @@ const SettingsDrawer = (props: { open: boolean }) => {
             <div
                 id="drawer-content"
             >
+                {import.meta.env.MODE === "development" && <HomeButton/>}
                 <LanguagePicker />
                 <ThemePicker />
             </div>
