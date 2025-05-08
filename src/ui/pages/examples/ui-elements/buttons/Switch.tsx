f8d0e139ac96fa33c32bf63cb38491509ad0c0fe
Toni Cantarella <toni.cantarella@outlook.com>
2025-05-08 19:20:55 +0300

    Confetti for the switch button

--- src/ui/pages/examples/ui-elements/buttons/Switch.tsx
+++ src/ui/pages/examples/ui-elements/buttons/Switch.tsx
@@ -1,28 +1,65 @@
+import { useState } from "react"
 import { useTranslation } from "react-i18next"
 import "./Switch.scss"
-import { useState } from "react"
 
 export const Switch = () => {
     const [on, setOn] = useState<boolean>(false)
     const { t } = useTranslation()
 
     return (
-        <button
-            id="switch"
-            className={`${on ? "on" : ""}`}
-            onClick={() => setOn(!on)}
-        >
-            <div id="track">
-                <span id="on">
-                    {t("examples.switch-on")}
-                </span>
-                <span id="off">
-                    {t("examples.switch-off")}
-                </span>
-                <div
-                    id="handle"
-                />
-            </div>
-        </button>
+        <>
+            {on &&
+                <Confetti />
+            }
+
+            <button
+                id="switch"
+                className={`${on ? "on" : ""}`}
+                onClick={() => setOn(!on)}
+            >
+                <div id="track">
+                    <span id="on">
+                        {t("examples.switch-on")}
+                    </span>
+                    <span id="off">
+                        {t("examples.switch-off")}
+                    </span>
+                    <div
+                        id="handle"
+                    />
+                </div>
+            </button>
+        </>
     )
 }
+
+const Confetti = () => {
+    const amount = 20
+
+    const confettis = [
+        "🌸",
+        "🏵️",
+        "💮",
+        "🌺"
+    ]
+    const confettiIndex = Math.floor(Math.random() * confettis.length)
+
+    return (
+        <>
+            {Array.from({ length: amount }).map((_, index) => (
+                <span
+                    key={index}
+                    className="confetti"
+                    style={{
+                        left: `${index * 5}%`,
+                        animationDelay: `${Math.random() * index}s`
+                    }}
+                >
+                    {confettis[confettiIndex]}
+                </span>
+            ))}
+        </>
+    )
+}
+
+//Math.random() * (100 - index) + index
