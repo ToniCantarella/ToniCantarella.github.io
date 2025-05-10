cefb1ce5ce0049f727c77f481cf52dfc02fa5ad9
Toni Cantarella <toni.cantarella@outlook.com>
2025-05-10 10:42:55 +0300

    Snackbar fully working as expected

--- src/ui/pages/examples/ui-elements/buttons/Ripple.tsx
+++ src/ui/pages/examples/ui-elements/buttons/Ripple.tsx
@@ -7,7 +7,7 @@ export const RippleButton = () => {
     const [snack, setSnack] = useState<number>(-1)
     const { t } = useTranslation()
 
-    const messages = Array.from({length: 7}).map((_, index) => (
+    const messages = Array.from({ length: 7 }).map((_, index) => (
         t(`examples.snackbar-message-${index}`)
     ))
 
@@ -55,39 +55,53 @@ export const RippleButton = () => {
     )
 }
 
-type Snack = { id: number; message: string };
+type Snack = { id: number, message: string, remove?: boolean, ready?: boolean }
 
 const Snackbar = (props: { snack?: string }) => {
     const maxSnacks = 3
     const [visibleSnacks, setVisibleSnacks] = useState<Snack[]>([])
-    const [idToRemove, setIdToRemove] = useState<number>(0)
 
     useEffect(() => {
         if (props.snack) {
             setVisibleSnacks(prev => {
-                if (visibleSnacks.length === maxSnacks) {
-                    const [firstSnack] = visibleSnacks
-                    setIdToRemove(firstSnack.id)
-                    return prev
-                } else
-                    return [...prev, { id: Date.now(), message: props.snack! }]
+                const activeSnacks = prev.filter(snack => !snack.remove)
+                const newSnacks = [...prev]
+
+                if (activeSnacks.length >= maxSnacks) {
+                    const indexToRemove = prev.findIndex(snack => !snack.remove)
+                    if (indexToRemove !== -1) {
+                        newSnacks[indexToRemove] = { ...newSnacks[indexToRemove], remove: true }
+                    }
+                }
+
+                return [...newSnacks, { id: Date.now(), message: props.snack! }]
             })
         }
     }, [props.snack])
 
     useEffect(() => {
         const interval = setInterval(() => {
-            if (visibleSnacks.length > 0) {
-                const [firstSnack] = visibleSnacks
-                setIdToRemove(firstSnack.id)
-            }
-        }, 4000);
-        return () => clearInterval(interval);
-    }, [visibleSnacks.length])
+            setVisibleSnacks(prev => {
+                const indexToRemove = prev.findIndex(snack => !snack.remove)
+                if (indexToRemove !== -1) {
+                    const newSnacks = [...prev]
+                    newSnacks[indexToRemove] = { ...newSnacks[indexToRemove], remove: true }
+                    return newSnacks
+                }
+                return prev
+            })
+        }, 4000)
+        return () => clearInterval(interval)
+    }, [])
 
-    const removeSnack = (idToRemove: number) => {
-        setVisibleSnacks(prev => prev.filter(snack => snack.id !== idToRemove))
-    };
+    const deleteSnack = (id: number) => {
+        setVisibleSnacks(prev => prev.filter(snack => snack.id !== id))
+    }
+
+    const onAnimationEnd = (animationName: string, snackId: number) => {
+        if (animationName === "snackIn") setVisibleSnacks(prev => prev.map(snack => snack.id === snackId ? { ...snack, ready: true } : snack))
+        if (animationName === "snackOut") deleteSnack(snackId)
+    }
 
     return createPortal(
         <div className="snackbar">
@@ -95,12 +109,10 @@ const Snackbar = (props: { snack?: string }) => {
                 <div
                     key={snack.id}
                     className="snack"
-                    onAnimationEnd={e => {
-                        if (e.animationName === "snackOut") removeSnack(snack.id);
-                    }}
+                    onAnimationEnd={(e) => onAnimationEnd(e.animationName, snack.id)}
                     style={{
                         top: `calc(${visibleSnacks.length - (index + 1)} * var(--snackbar-gap))`,
-                        animationName: snack.id === idToRemove ? "snackOut" : "snackIn"
+                        animationName: snack.remove && snack.ready ? "snackOut" : "snackIn",
                     }}
                 >
                     <span>{snack.message}</span>
