a3213bf8e750f8ec4190c4c26d668725798c30bd
Toni Cantarella <toni.cantarella@outlook.com>
2025-05-10 11:09:23 +0300

    Slider input element shows an emoji

--- src/ui/pages/examples/ui-elements/input/Slider.tsx
+++ src/ui/pages/examples/ui-elements/input/Slider.tsx
@@ -5,12 +5,46 @@ export const Slider = () => {
     const min = 0
     const max = 100
     const [value, setValue] = useState<number>(max / 2)
+    const [currentEmoji, setCurrentEmoji] = useState<number>(0)
 
+    const emojis = [
+        "🤥",
+        "🫨",
+        "🤠",
+        "🤯",
+        "😮‍💨",
+    ]
 
     return (
         <div className="slider">
-            <input type="range" min={min} max={max} value={value} onChange={event => setValue(Number(event.target.value))} />
-            <span>{value}</span>
+            <span
+                className="background-element"
+                style={{
+                    opacity: `${value / max}`,
+                    rotate: `${720 * (value / max)}deg`,
+                    scale: `${(value* 5) / max}`
+                }}
+            >
+                {emojis[currentEmoji]}
+            </span>
+
+            <span
+                className="value"
+            >
+                {value}
+            </span>
+            <input
+                type="range"
+                min={min}
+                max={max}
+                value={value}
+                onChange={event => {
+                    setValue(Number(event.target.value))
+                    if (Number(event.target.value) < 1) {
+                        setCurrentEmoji(prev => (prev + 1) % emojis.length)
+                    }
+                }}
+            />
         </div>
     )
 }
