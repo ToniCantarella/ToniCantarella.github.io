81c2623f16baa3880cbf9197d085ca4bbfc8100f
Toni Cantarella <toni.cantarella@outlook.com>
2026-01-06 08:52:50 +0200

    Localization files under one folder

--- src/localization/i18n.ts
+++ src/localization/i18n.ts
@@ -1,8 +1,8 @@
 import i18n from 'i18next'
 import LanguageDetector from 'i18next-browser-languagedetector'
 import { initReactI18next } from 'react-i18next'
-import en from '../locales/en/translation.json'
-import fi from '../locales/fi/translation.json'
+import en from './locales/en/translation.json'
+import fi from './locales/fi/translation.json'
 import { SupportedLanguages } from '../ui/language-picker/LanguagePicker'
 
 i18n
