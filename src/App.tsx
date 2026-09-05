58fced7e0420715ecf339f04ee5e99e980b840eb
Toni Cantarella <toni.cantarella@outlook.com>
2026-09-05 08:39:27 +0300

    Removed navigation hooks that were unused

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,14 @@
-import { Outlet, useNavigationType } from 'react-router'
+import { Outlet } from 'react-router'
 import NavBar from './ui/navigation/NavBar'
 
 function App() {
-  const navigationType = useNavigationType()
-
-  const direction =
-        navigationType === "PUSH"
-            ? "forward"
-            : navigationType === "POP"
-                ? "backward"
-                : "none"
 
   return (
     <div className='flex flex-col h-full'>
       <NavBar
         onLogoClick={() => { }}
       />
-      <main className={`page-content flex flex-1 navigation-${direction}`}>
+      <main className="page-content flex flex-1">
         <Outlet />
       </main>
     </div>
