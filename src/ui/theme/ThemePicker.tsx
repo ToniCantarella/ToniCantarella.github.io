046442edec7dd19b6d747d19c56e772d6f63327d
tonicantarella <toni.cantarella@sensire.com>
2026-09-12 13:52:56 +0300

    Theme is saved to localstorage

--- src/ui/theme/ThemePicker.tsx
+++ src/ui/theme/ThemePicker.tsx
@@ -3,10 +3,12 @@ import {useEffect, useState} from "react";
 const themes = ["light", "dark"]
 
 export default function ThemePicker() {
-    const [theme, setTheme] = useState<string>("")
+    const fetchedTheme = localStorage.getItem("theme")
+    const [theme, setTheme] = useState<string>(fetchedTheme ? fetchedTheme : "dark");
 
     useEffect(() => {
         document.documentElement.setAttribute("data-theme", theme)
+        localStorage.setItem("theme", theme)
     }, [theme]);
 
     return (
