91d3af6ed72f7eb6f77141e8015b72447bf133f2
tonicantarella <toni.cantarella@sensire.com>
2026-09-15 05:25:09 +0300

    Translated short introduction to about page

--- src/ui/home/Home.tsx
+++ src/ui/home/Home.tsx
@@ -3,10 +3,16 @@ import {useTranslation} from "react-i18next";
 
 export default function Home() {
     const {t} = useTranslation()
+    const now = new Date()
+    const firstDayOfWork = new Date("2022-05-30")
+    const years = now.getFullYear() - firstDayOfWork.getFullYear()
+    const months = now.getMonth() - firstDayOfWork.getMonth()
+    const monthsString = months > 6 ? ",5" : ""
+    const yearsOfExperience = `${years}${monthsString}`
 
     return (
         <Page>
-            {t("home.introduction")}
+            {t("home.introduction", {yearsOfExperience: yearsOfExperience})}
         </Page>
     )
 }
